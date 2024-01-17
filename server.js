ebb11d7e9dbdfc65b56dc5dcff25261fc614ef78
a-rudolph <adam.rudolph@live.ca>
2024-01-16 20:06:47 -0500

    use ejs for templating

--- server.js
+++ server.js
@@ -4,8 +4,38 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+const users = [
+  {
+    id: 1,
+    name: "John",
+    email: "",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    email: "",
+  },
+  {
+    id: 3,
+    name: "Joe",
+    email: "",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index", {
+    title: "index",
+  });
+});
+
+app.get("/users", (req, res) => {
+  res.render("users", {
+    title: "users",
+    users,
+  });
 });
 
 app.listen(port, () => {
